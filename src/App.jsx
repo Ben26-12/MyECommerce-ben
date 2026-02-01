2588ebdc2865a6fa49c402846259a191cc30c210
Ben Nguyen <nguyenkhacly2612@gmail.com>
2026-02-01 23:38:28 +0700

    base structure creating

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import { useState } from "react";
 
+import "@styles/main.scss";
+import MainLayout from "@components/Layout";
+import Header from "@components/Header/";
+import Footer from "@components/Footer/";
 function App() {
-  return <div>welcome Ben</div>;
+  return (
+    <>
+      <h1>Welcome Ben</h1>
+      <MainLayout>
+        <Header />
+        My content
+        <Footer />
+      </MainLayout>
+    </>
+  );
 }
 export default App;
